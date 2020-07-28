19f23a7b7353d9951574d792df679fc26a066432
WST <ilya.averkov@gmail.com>
2020-07-28 22:42:17 +0300

    Убрал черновик fakezonegen, так как больше не планируется

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ fakeroot dpkg-deb --build rkn-load
 fakeroot dpkg-deb --build rkn-check
 fakeroot dpkg-deb --build rkn-parse
 fakeroot dpkg-deb --build rkn-cronjobs
-fakeroot dpkg-deb --build rkn-fakezonegen
 fakeroot dpkg-deb --build rkn-status
